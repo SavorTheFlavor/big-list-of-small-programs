c66c88bf6d6fd45eca4e766c8c993ab5575ffb08
pritam <pritam42069@protonmail.com>
2022-05-30 10:18:18 +0530

    remove bracket

--- Programs/05.py
+++ Programs/05.py
@@ -3,7 +3,7 @@ import random
 def int_to_timestamp(number: int = random.randint(1, 1000)):
     hh = number // 3600
     mm = (number - hh*3600)//60
-    ss = ((number - hh*3600) - mm*60)
+    ss = (number - hh*3600) - mm*60
     return f"{hh}:{mm}:{ss}"
 
 # example
